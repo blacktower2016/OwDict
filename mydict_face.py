40b1000dbfee97e8904d99768055809ec6bea50c
mxx <mlyn@tut.by>
2017-01-15 14:20:56 +0300

    Initial commit for tkinterUI branch

---
+++ mydict_face.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from mydict_back import Dictionary
+from tkinter import *
+
+'''This is tkinter UI for owdict (mydict_back.py)
+'''
+
+root=Tk()
+root.resizable(False,False)
+mydict = Dictionary("my_en_ru")
+
+def p(event): #on window destroy saves dictionary file
+    #root.configure(cursor="watch")
+    mydict.save()
+    #root.configure(cursor="arrow")
+    
+root.bind('<Destroy>',p)
+
+#def find_word(event): #for bind with event
+def find_word():
+    #root.configure(cursor="watch")
+    trans.set(mydict.find_word(word.get()))
+    #root.configure(cursor="arrow")
+
+word=StringVar()
+trans=StringVar()
+	
+wordFrame = LabelFrame(root)
+wordFrame.pack(side = 'top')
+transFrame = LabelFrame(root)
+transFrame.pack(side = 'bottom')
+txtWord = Entry(wordFrame, textvariable = word)	
+txtWord.pack(side = 'left')
+btn = Button(wordFrame, text="Find Word", bg="white", fg="black", command = find_word ) #width=10, height=2
+#btn.bind("<Button-1>", find_word)
+btn.pack(side = 'right')
+lblTrans = Label(transFrame, bg="White", fg='black', textvariable=trans, font=('Times New Roman', 20), width = 25)
+lblTrans.pack()
+
+root.mainloop()
